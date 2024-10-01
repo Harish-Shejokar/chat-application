4aa2d8f1b84e738f2b3dcfeb61ab12ba21822a28
Harish <hshejokar@gmail.com>
2024-10-01 18:05:28 +0530

    disconnect & send-message feature

--- index.js
+++ index.js
@@ -21,13 +21,24 @@ const io = new Server(6001, {
     cors:true
 })
 
-io.on("connection",  (socket) => {
+io.on("connect",  (socket) => {
     console.log("new connecton", socket.id)
     socket.on("joined",({user})=>{
         console.log(`${user} has joined`);
         allUsers[socket.id] = user; 
-        socket.broadcast.emit("userJoined", {user:"Admin", message:`${allUsers[socket.id]} has joined`});
+        socket.broadcast.emit("userJoined", {user:`${allUsers[socket.id]}`, message:`joined-Chat`, id:allUsers[socket.id]});
     })
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("leave",{user:"", message:`${allUsers[socket.id]} leave the chat`});
+    })
+
+    socket.on("message", ({message,id}) => {
+        io.emit("sendMessage", {user : allUsers[id], message});
+    })
+
 })
 
 const port = process.env.PORT || 4500;
+
+// server.listen(port, ()=> console.log(`http server listen on port - ${port}`))
